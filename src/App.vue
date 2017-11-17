6e960cb070cb1493b53db4b8cf4e87436eff14a2
Luís Felipe Zanatto Alves <luisfelipe.zanatto@gmail.com>
2017-11-17 18:54:02 -0200

    :tada: project done

---
+++ src/App.vue
@@ -0,0 +1,107 @@
+<template>
+  <div id="app">
+    <div class="navbar has-shadow">
+      <div class="container">
+
+        <div class="navbar-brand">
+          <a class="navbar-item">felzan</a>
+        </div>
+
+        <span class="navbar-burger" v-on:click="toggleNavbar" v-bind:class="{'is-active': isActive}">
+          <span></span>
+          <span></span>
+          <span></span>
+        </span>
+
+        <div class="navbar-menu" v-bind:class="{'is-active': isActive}">
+          <div class="navbar-end">
+            <router-link to="/" class="navbar-item r-item">Home</router-link>
+            <router-link to="/faq" class="navbar-item r-item">Faq</router-link>
+            <router-link to="/faq" class="navbar-item r-item">Home</router-link>
+            <router-link to="/faq" class="navbar-item r-item">Home</router-link>
+          </div>
+
+          <div class="navbar-item">
+            <p class="control">
+              <a href="" class="button is-primary is-outlined">
+                <span class="icon">
+                  <i class="fa fa-download"></i>
+                </span>
+                <span>
+                  Join now
+                </span>
+              </a>
+            </p>
+          </div>
+        </div>
+
+      </div>
+    </div>
+
+    <router-view/>
+
+    <footer class="footer is-primary">
+      <div class="container">
+        <div class="columns">
+          <div class="column">
+            <p>Similique voluptas pariatur velit perferendis.</p>
+          </div>
+          <div class="column has-text-right">
+            <a href="#" class="icon"><i class="fa fa-facebook"></i></a>
+            <a href="#" class="icon"><i class="fa fa-twitter"></i></a>
+          </div>
+        </div>
+      </div>
+    </footer>
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'app',
+  data: function() {
+    return {
+      isActive: false
+    }
+  },
+  methods: {
+    toggleNavbar: function() {
+      this.isActive = !this.isActive
+    }
+  }
+}
+</script>
+
+<style lang="sass">
+@import '../node_modules/bulma/bulma.sass'
+@import 'mq.sass'
+
+.navbar
+  background-color: #383838
+  a:hover
+    color: gray
+
+.navbar-brand a
+  color: #fff
+  font-weight: bold
+
+a.r-item
+  color: #c1c1c1
+  padding: 0.5rem 1.75rem
+  +mobile
+    color: gray
+    &:hover
+      background-color: #f1f1f1
+
+// .navbar-menu span
+//   background-color: #c1c1c1
+
+footer
+  background-color: $primary !important
+  color: #fff
+
+  .icon
+    color: #fff
+    margin-left: 20px
+</style>
---
+++ src/components/Home.vue
@@ -0,0 +1,105 @@
+<template>
+  <div class="home">
+    <section class="hero">
+      <div class="hero-body">
+        <div class="container">
+          <h1 class="title">
+            {{ heading }}
+            <div class="is-two-thirds column is-paddingless">
+              <h2 class="subtitle is-4">
+                {{ subheading }}
+              </h2>
+            </div>
+            <a href="" class="button is-large is-primary" id="learn">
+              Learn more
+            </a>
+          </h1>
+        </div>
+      </div>
+    </section>
+    
+    <section class="section">
+      <div class="containet">
+        <div class="columns pd is-desktop">
+          <div class="columns is-1 has-text-centered">
+            <i class="fa fa-cog is-primary"></i>
+          </div>
+          <div class="columns is-one-third-desktop">
+            <p class="title"><strong>Maxime doloremque omnis.</strong></p>
+          </div>
+          <div class="columns is-one-third-desktop">
+            <p>Veritatis nesciunt necessitatibus doloribus tempore. Magni sint dolores est voluptatem est quisquam dolor quaerat inventore. Saepe et impedit et a sit earum et consequatur ut. Sit placeat et assumenda similique aut doloribus. Nobis nihil autem quibusdam. Veniam impedit magni laudantium ad iste.</p>
+          </div>
+        </div>
+      </div>
+
+      <div class="columns pd">
+        <div class="column">
+           <div class="card">
+             <div class="card-content">
+               <p class="title">Eaque laborum dolores.Eaque laborum dolores.Eaque laborum dolores.</p>
+               <p class="subtitle">Gary Simon</p>
+             </div>
+           </div>
+        </div>
+        <div class="column">
+           <div class="card">
+             <div class="card-content">
+               <p class="title">necessitatibus consequatur qui</p>
+               <p class="subtitle">Gary Simon</p>
+             </div>
+           </div>
+        </div>
+        <div class="column">
+           <div class="card">
+             <div class="card-content">
+               <p class="title">necessitatibus consequatur qui</p>
+               <p class="subtitle">Gary Simon</p>
+             </div>
+           </div>
+        </div>
+      </div>
+
+    </section>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'home',
+  data() {
+    return {
+      heading: 'Soaring to new heights',
+      subheading: 'Inventore vel aut dolor eos amet aperiam molestiae. Maxime et sit eius laborum. Dolores labore culpa quia aut voluptatem dolores a blanditiis alias.'
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style lang="sass" scoped>
+@import '../mq'
+
+.hero
+  background-color: #e3e3e3
+  background-size: cover
+  
+  .title
+    +mobile
+      fint-wieght: bold
+    +tablet
+      font-size: 2.5rem
+    +desktop
+      font-size: 4rem
+      margin-top: 2.5rem
+
+h2
+  margin: 1.5rem 0 2rem 0 !important
+
+.fa-cog
+  font-size: 40px
+
+#learn
+  +desktop
+    margin-bottom: 2rem
+</style>
